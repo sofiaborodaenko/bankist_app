a11c2ee248706109aa64d4024ae312514a982be4
Sofia Borodaenko <soph.bor06@gmail.com>
2025-06-05 17:18:43 -0400

    fixed the date bug when sorting happens

--- starter/script.js
+++ starter/script.js
@@ -104,20 +104,35 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 // displays the main data in the center
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  // make a combined object that holds the movments with the dates
+  const combinedMovsDates = acc.movements.map((mov, i) => ({
+    movement: mov,
+    movementDate: acc.movementsDates.at(i),
+  }));
 
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? "deposit" : "withdrawal";
+  if (sort) combinedMovsDates.sort((a, b) => a.movement - b.movement);
+
+  combinedMovsDates.forEach(function (obj, i) {
+    const { movement, movementDate } = obj;
+    const type = movement > 0 ? "deposit" : "withdrawal";
+
+    const date = new Date(movementDate);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    const displayDate = `${day}/${month}/${year}`;
 
     const html = `
         <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${mov.toFixed(2)}€</div>
+          <div class="movements__date">${displayDate}</div>
+          <div class="movements__value">${movement.toFixed(2)}€</div>
         </div>
         `;
 
@@ -174,7 +189,7 @@ createUsername(accounts);
 
 function updateUI(acc) {
   // displays movement
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // displays balance
   calcDisplayBalance(acc);
@@ -185,6 +200,11 @@ function updateUI(acc) {
 
 //--------------EVENT HANDLERS--------------
 let currAccount;
+
+// fake log in
+currAccount = account1;
+updateUI(currAccount);
+containerApp.style.opacity = 1;
 
 btnLogin.addEventListener("click", function (e) {
   e.preventDefault();
@@ -208,6 +228,15 @@ btnLogin.addEventListener("click", function (e) {
 
     // update the UI
     updateUI(currAccount);
+
+    // create the date
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hour = `${now.getHours()}`.padStart(2, 0);
+    const min = `${now.getMinutes()}`.padStart(2, 0);
+    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
   }
 });
 
@@ -232,6 +261,11 @@ btnTransfer.addEventListener("click", function (e) {
     // doing the transfer
     currAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
+
+    // add transfer date
+    currAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     updateUI(currAccount);
   }
 });
@@ -249,6 +283,9 @@ btnLoan.addEventListener("click", function (e) {
   ) {
     // adding movemenet
     currAccount.movements.push(ammount);
+
+    // add loan date
+    currAccount.movementsDates.push(new Date().toISOString());
 
     //update UI
     updateUI(currAccount);
@@ -285,6 +322,6 @@ btnClose.addEventListener("click", function (e) {
 let sorted = false;
 btnSort.addEventListener("click", function (e) {
   e.preventDefault;
-  displayMovements(currAccount.movements, !sorted);
+  displayMovements(currAccount, !sorted);
   sorted = !sorted;
 });
